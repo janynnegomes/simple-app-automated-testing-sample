6d8da8768463ecae4dc99812e983154ba17994b6
janynnegomes <janynnegomes@gmail.com>
2020-08-01 19:20:23 -0300

    added mock for basic static data and renderizationtesting of form

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,6 +1,10 @@
 import { TestBed, async } from '@angular/core/testing';
 import { AppComponent } from './app.component';
 
+const mock={
+  title:'Simple App'
+}
+
 describe('AppComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
@@ -19,13 +23,28 @@ describe('AppComponent', () => {
   it(`should have as title 'Simple App'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('Simple App');
+    expect(app.title).toEqual(mock.title);
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.toolbar span').textContent).toContain('Simple App');
+    expect(compiled.querySelector('.toolbar span').textContent).toContain(mock.title);
   });
+
+  
+  it('should render a form for new task', () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.componentInstance;  
+    fixture.detectChanges();
+
+    const compiled = fixture.nativeElement;
+    
+    expect(compiled.querySelector('#formNewTask input#title')).toBeTruthy()
+    expect(compiled.querySelector('#formNewTask textarea#description')).toBeTruthy()
+    expect(compiled.querySelector('#formNewTask input#btnSaveTask')).toBeTruthy()
+ 
+  });
+  
 });
